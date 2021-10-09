855eb905a59f694d7aa38082596e6a857b08bb7c
Hashem Alkeshawi <hashemalkeshawi@gmail.com>
2021-10-09 19:58:01 +0300

    second version of the site!

--- ShowinList_script.js
+++ ShowinList_script.js
@@ -1,7 +1,7 @@
 var isClicked = false;
+var isScrolled = 0;
 function onEnter() {
     var list = document.getElementById("nav_list");
-        list.style.transitionProperty="0.8s"
         list.style.display = "inline";
     
 }
@@ -23,5 +23,26 @@ function onItemSelect(){
         isClicked = !(isClicked);
         list.style.display = "none";
     }
+}
+
+function showBack(){
+    isScrolled++;
+    if(isScrolled>0){
+    var upArrow = document.getElementById("home_back");
+        upArrow.style.display = "block";
+        isScrolled = 0;
+
+    }
+
+    
+}
+function hideBack(){
+    isScrolled = -44;
+    var upArrow = document.getElementById("home_back");
+    window.location.href = "#body_top";
+    upArrow.style.display = "none";
+
+
+    
 }
 
